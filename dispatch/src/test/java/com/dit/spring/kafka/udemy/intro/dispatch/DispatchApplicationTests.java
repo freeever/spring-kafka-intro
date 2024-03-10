a9d0acc21cd2a20b2c9c782e807c8d39b3116e95
Daniel Xu <zhiqiang.xu2015@gmail.com>
2024-03-10 13:54:22 -0400

    #1 - Initial commit

---
+++ dispatch/src/test/java/com/dit/spring/kafka/udemy/intro/dispatch/DispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.spring.kafka.udemy.intro.dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
